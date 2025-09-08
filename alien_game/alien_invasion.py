811862012e60f277fdd0a1f138eff350efa0d3a0
adrianor85 <adrianorosa085@gmail.com>
2025-09-08 15:30:03 -0300

    bullet class

--- alien_game/alien_invasion.py
+++ alien_game/alien_invasion.py
@@ -3,6 +3,9 @@ import pygame as pg
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
+
+
 class AlienInvasion:
 
   def __init__(self) -> None:
@@ -13,11 +16,13 @@ class AlienInvasion:
     pg.display.set_caption("Alien Invasion")
 
     self.ship = Ship(self)
+    self.bullets = pg.sprite.Group()
 
   def run_game(self):
     while True:
       self._check_event()
       self.ship.moving_ship()
+      self._update_bullet()
       self._update_screen()
       self.clock.tick(self.settings.fps)
 
@@ -38,6 +43,8 @@ class AlienInvasion:
       self.ship.moving_left = True
     elif event.key == pg.K_q:
       sys.exit()
+    elif event.key == pg.K_SPACE:
+      self._fire_bullet()
 
 
   def _check_keyup_event(self, event):
@@ -47,8 +54,26 @@ class AlienInvasion:
       self.ship.moving_left = False
 
 
+  def _fire_bullet(self):
+    """Create a new bullet and add it to the bullets group."""
+    if len(self.bullets) < self.settings.bullets_allowed:
+      new_bullet = Bullet(self)
+      self.bullets.add(new_bullet)
+
+  def _update_bullet(self):
+    """Update position of bullets and get rid of old bullets."""
+    # Update bullet positions.
+    self.bullets.update()
+
+    # Get rid of bullets that have disappeared.
+    for bullet in self.bullets.copy():
+      if bullet.rect.bottom <= 0:
+        self.bullets.remove(bullet)
+
   def _update_screen(self):
     self.screen.fill(self.settings.bg_color)
+    for bullet in self.bullets.sprites():
+      bullet.draw_bullet()
     self.ship.draw_ship()
     pg.display.flip()
     
